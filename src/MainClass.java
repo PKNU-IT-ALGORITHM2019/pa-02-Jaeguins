27bbad15e94b2ae1019acd8daea5c4a368a5da4e
SangWooJeong <jsu4838@naver.com>
2019-03-21 16:10:49 +0900

    Memoization 추가
    새 결과로 MD 갱신

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 public class MainClass {
     int N;
     boolean solved=false;
@@ -8,6 +9,7 @@ public class MainClass {
     double minDist=Double.MAX_VALUE;
     int[] stack;
     int[] result;
+    double[][]memo;
     public static void main(String args[]){
         for(int T=0;T<=6;T++){
             MainClass main = new MainClass();
@@ -23,6 +25,7 @@ public class MainClass {
                 }
                 fileScanner.close();
                 long StartTime=System.currentTimeMillis();
+                main.memo=new double[main.N][main.N];
                 main.calc(0, 0);
                 long EndTime=System.currentTimeMillis();
                 for (int i = 0; i < main.N; i++)
@@ -33,12 +36,14 @@ public class MainClass {
             }
         }
     }
-    double getDist(int from[],int to[]){
-        if(from==null)
-            return 0;
-        double xDiff=from[0]-to[0];
-        double yDiff=from[1]-to[1];
-        return Math.sqrt(xDiff*xDiff+yDiff*yDiff);
+    double getDist(int from,int to){
+        if(memo[from][to]==0){
+            double xDiff=field[from][0]-field[to][0];
+            double yDiff=field[from][1]-field[to][1];
+            return memo[from][to]=Math.sqrt(xDiff*xDiff+yDiff*yDiff);
+        }else{
+            return memo[from][to];
+        }
     }
     void swap(int i,int j){
         int t=stack[i];
@@ -49,8 +54,8 @@ public class MainClass {
         if(acc>minDist)return;
         if(depth==N-1){
             double tDist=acc+
-                    getDist(field[stack[depth-1]],field[stack[depth]])+
-                    getDist(field[stack[0]],field[stack[depth]]);
+                    getDist(stack[(depth-1==-1?0:depth-1)],stack[depth])+
+                    getDist(stack[0],stack[depth]);
             if(tDist<minDist){
                 result=stack.clone();
                 minDist=tDist;
@@ -59,7 +64,7 @@ public class MainClass {
         }
         for (int i = depth; i < N; i++) {
             swap(i, depth);
-            calc(depth + 1,acc+getDist(depth>0?field[stack[depth-1]]:null,field[stack[depth]]));
+            calc(depth + 1,acc+getDist(depth>0?stack[depth-1]:0,stack[depth]));
             swap(i, depth);
         }
     }
